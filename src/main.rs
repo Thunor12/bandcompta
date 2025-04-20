e23ea485b5d6fe1c3e9cafbe988025053aeacca3
lucasspiteri <lucasspiteri@protonmail.com>
2025-04-20 23:57:48 +0200

    leart how to use callbacks

--- src/main.rs
+++ src/main.rs
@@ -1,99 +1,8 @@
-use chrono::prelude::*;
-use serde_derive::{Deserialize, Serialize};
-use yew::{html::ImplicitClone, prelude::*};
+use web_sys::wasm_bindgen::JsCast;
+use web_sys::{EventTarget, HtmlInputElement};
+use yew::prelude::*;
 
-// type Date_t = DateTime<Utc>;
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq)]
-enum TransactionType {
-    INCOME,
-    EXPENSE,
-    NDF,
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
-struct Transaction {
-    name: String,
-    company: String,
-    transaction_type: TransactionType,
-    executed: bool,
-    date: String,
-    price_full_tax: f32,
-    tag: String,
-    tax_amount: f32,
-    invoice_path: String,
-}
-impl ImplicitClone for Transaction {}
-
-#[derive(Properties, PartialEq)]
-struct TransactionTableRowProps {
-    transaction: Transaction,
-}
-
-#[function_component(TransactionTableRow)]
-fn transaction_table_row(
-    TransactionTableRowProps { transaction }: &TransactionTableRowProps,
-) -> Html {
-    html! {
-        <tr>
-            <td> {format!("{}",   &transaction.name)              } </td>
-            <td> {format!("{}",   &transaction.company)           } </td>
-            <td> {format!("{:?}", &transaction.transaction_type)  } </td>
-            <td> {format!("{}",   &transaction.executed)          } </td>
-            <td> {format!("{}",   &transaction.date)              } </td>
-            <td> {format!("{}",   &transaction.price_full_tax)    } </td>
-            <td> {format!("{}",   &transaction.tag)               } </td>
-            <td> {format!("{}",   &transaction.tax_amount)        } </td>
-            <td> {format!("{}",   &transaction.invoice_path)      } </td>
-        </tr>
-    }
-}
-
-const TIME_FMT: &'static str = "%Y-%m-%d";
-
-#[derive(Properties, PartialEq)]
-struct TransactionListProps {
-    transactions: Vec<Transaction>,
-}
-
-#[function_component(TransactionList)]
-fn transaction_list(TransactionListProps { transactions }: &TransactionListProps) -> Html {
-    let col_name = "name ";
-    let col_company = "company ";
-    let col_transaction_type = "transaction type ";
-    let col_executed = "executed ";
-    let col_date = "date ";
-    let col_price_full_tax = "price full tax ";
-    let col_tag = "tag ";
-    let col_tax_amount = "tax amount ";
-    let col_invoice_path = "invoice path";
-
-    let h = html! {
-        <table>
-            <tr>
-                <th> {col_name}             </th>
-                <th> {col_company}          </th>
-                <th> {col_transaction_type} </th>
-                <th> {col_executed}         </th>
-                <th> {col_date}             </th>
-                <th> {col_price_full_tax}   </th>
-                <th> {col_tag}              </th>
-                <th> {col_tax_amount}       </th>
-                <th> {col_invoice_path}     </th>
-            </tr>
-                {
-                    transactions
-                        .iter()
-                        .map(|tr| {html! {<TransactionTableRow transaction={tr} />}})
-                        .collect::<Html>()
-                }
-        </table>
-    };
-
-    h
-}
-
-#[derive(Clone, Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Clone, PartialEq)]
 struct Video {
     id: usize,
     title: String,
@@ -101,40 +10,130 @@ struct Video {
     url: String,
 }
 
+#[derive(Properties, PartialEq)]
+struct VideosListProps {
+    videos: Vec<Video>,
+    on_click: Callback<Video>,
+}
+
+#[function_component(VideosList)]
+fn videos_list(VideosListProps { videos, on_click }: &VideosListProps) -> Html {
+    let on_click = on_click.clone();
+
+    videos
+        .iter()
+        .map(|video| {
+            let on_video_select = {
+                let on_click = on_click.clone();
+                let video = video.clone();
+                Callback::from(move |e: Event| {
+                    // When events are created the target is undefined, it's only
+                    // when dispatched does the target get added.
+                    let target: Option<EventTarget> = e.target();
+                    let input = target.and_then(|t| t.dyn_into::<HtmlInputElement>().ok());
+
+                    if let Some(input) = input {
+                        // input_value_handle.set(input.value());
+                        let mut v = video.clone();
+                        v.title = input.value();
+                        on_click.emit(v.clone())
+                    }
+                })
+            };
+
+            // <p key={video.id} onclick={on_video_select}>{format!("{}: {}", video.speaker, video.title)}</p>
+            html! {
+                // <button onclick={on_video_select}>
+                // { "Click me!" }
+                // </button>
+
+                <input onchange={on_video_select}
+                    id="cautious-input"
+                    type="text"
+                    value={video.title.clone()}
+                />
+            }
+        })
+        .collect()
+}
+
+#[derive(Properties, PartialEq)]
+struct VideosDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideosDetailsProps { video }: &VideosDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{ video.title.clone() }</h3>
+            <img src="https://placehold.co/640x360.png?text=Video+Player+Placeholder" alt="video thumbnail" />
+        </div>
+    }
+}
+
 #[function_component(App)]
 fn app() -> Html {
-    let trs = vec![
-        Transaction {
-            name: "Logo Athas 1".into(),
-            company: "Moonroot".into(),
-            transaction_type: TransactionType::EXPENSE,
-            date: "2025-01-01".into(),
-            price_full_tax: 200.0,
-            tax_amount: 20.0,
-            executed: true,
-            tag: "Visuels".into(),
-            invoice_path: "./".into(),
-        },
-        Transaction {
-            name: "Cachet Klub".into(),
-            company: "Klub".into(),
-            transaction_type: TransactionType::INCOME,
-            date: "2025-01-01".into(),
-            price_full_tax: 50.0,
-            tax_amount: 0.0,
-            executed: true,
-            tag: "Visuels".into(),
-            invoice_path: "./".into(),
-        },
-    ];
+    let selected_video = use_state(|| None);
+
+    let videos = use_state(|| vec![]);
+    {
+        let videos = videos.clone();
+        use_effect_with((), move |_| {
+            let videos = videos.clone();
+            {
+                let t_videos = vec![
+                    Video {
+                        id: 1,
+                        title: "Building and breaking things".to_string(),
+                        speaker: "John Doe".to_string(),
+                        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+                    },
+                    Video {
+                        id: 2,
+                        title: "The development process".to_string(),
+                        speaker: "Jane Smith".to_string(),
+                        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+                    },
+                    Video {
+                        id: 3,
+                        title: "The Web 7.0".to_string(),
+                        speaker: "Matt Miller".to_string(),
+                        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+                    },
+                    Video {
+                        id: 4,
+                        title: "Mouseless development".to_string(),
+                        speaker: "Tom Jerry".to_string(),
+                        url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+                    },
+                ];
+                videos.set(t_videos.clone());
+            }
+            || ()
+        });
+    }
+
+    let on_video_select = {
+        let selected_video = selected_video.clone();
+        Callback::from(move |video: Video| selected_video.set(Some(video)))
+    };
+
+    let details = selected_video.as_ref().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()} />
+        }
+    });
 
     html! {
         <>
-            <h1>{ "Athas Band" }</h1>
+            <h1>{ "RustConf Explorer" }</h1>
             <div>
-                <h3>{"Transactions"}</h3>
-                <TransactionList transactions={trs} />
+                <h3>{"Videos to watch"}</h3>
+
+               <VideosList videos={(*videos).clone()} on_click={on_video_select.clone()} />
             </div>
+            { for details }
         </>
     }
 }
